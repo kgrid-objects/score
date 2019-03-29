23a33fd744219790cb914cf27ff8611a52507e38
Jack Allan <jackall@umich.edu>
2019-03-29 11:53:10 -0400

    added sumthin

--- score10yrRisk.js
+++ score10yrRisk.js
@@ -6,19 +6,19 @@ function score10yrRisk(inputs) {
     var chol = inputs.chol;
     var smoke = inputs.smoke;
     var risk = inputs.risk;
-    
-    
-    
+
+
+
     // attach error message if input arguments are out of range
 //    scoreArray.CHDRisk = "ERROR: Input out of range";
 //    scoreArray.NonCHDRisk = "ERROR: Input out of range";
 //    scoreArray.TotalRisk = "ERROR: Input out of range";
-//    
-   
+//
+
     var scoreArray = calculateRisk(age, sex, smoke, sbp, chol, risk);
-    
+
     return scoreArray;
-}    
+}
 
 function calculateRisk (age, sex, smoke, sbp, chol, risk) {
         // Output Array
@@ -28,7 +28,7 @@ function calculateRisk (age, sex, smoke, sbp, chol, risk) {
             TotalRisk: -5,
 
         };
-    
+
         //assign alpha and p coefficients
         var coefficientsCHD = {
              "low-m-alpha":0,
@@ -40,7 +40,7 @@ function calculateRisk (age, sex, smoke, sbp, chol, risk) {
              "high-f-alpha":0,
              "high-f-p":0
         }
-        
+
         var coefficientsNon = {
              "low-m-alpha":0,
              "low-m-p":1,
@@ -51,39 +51,39 @@ function calculateRisk (age, sex, smoke, sbp, chol, risk) {
              "high-f-alpha":0,
              "high-f-p":0
         }
-    
-       
+
+
         //calculate CHD Risk
         var alphaCHD = coefficientsCHD[risk + "-" + sex + "-alpha"];
         var pCHD = coefficientsCHD[risk + "-" + sex + "-p"];
         var w = ((0.24) * (chol - 6)) + ((0.018) * (sbp - 120)) + (0.71 * smoke);
-        
-            
+
+
             var s_sub_0_age = Math.exp(-1 * Math.exp(alphaCHD) * (Math.pow(age - 20),(pCHD)));
             var s_sub_0_age10 = Math.exp(-1 * Math.exp(alphaCHD) * (Math.pow(age - 10),(pCHD)));
-        
+
             var s_age = Math.pow(s_sub_0_age, Math.exp(w));
             var s_age10 = Math.pow(s_sub_0_age10, Math.exp(w));
-        
+
             //output
             scoreArray.CHDRisk = 1-(s_age10/s_age);
-        
-        
+
+
         //calculate non-CHD CVD Risk
         var alphaNon = coefficientsNon[risk + "-" + sex + "-alpha"];
         var pNon = coefficientsNon[risk + "-" + sex + "-p"];
         var wNon = ((0.02) * (chol - 6)) + ((0.022) * (sbp - 120)) + (0.63 * smoke);
-        
+
         var s_sub_0_ageNon = Math.exp(-1 * Math.exp(alphaNon) * (Math.pow(age - 20),(pNon)));
         var s_sub_0_age10Non = Math.exp(-1 * Math.exp(alphaNon) * (Math.pow(age - 10),(pNon)));
-        
+
             var s_ageNon = Math.pow(s_sub_0_ageNon, Math.exp(wNon));
             var s_age10Non = Math.pow(s_sub_0_age10Non, Math.exp(wNon));
-        
+
             //output
             scoreArray.NonCHDRisk = 1-(s_age10Non/s_ageNon);
             scoreArray.TotalRisk = scoreArray.CHDRisk + scoreArray.NonCHDRisk;
-            
-        
-        return scoreArray; 
+
+
+        return scoreArray;
 }
--- tests/score10yrRisk.test.js
+++ tests/score10yrRisk.test.js
@@ -1,8 +1,15 @@
-const score10yrRisk = require('../score10yrRisk');
+var rewire = require('rewire');
 
 var inputs = {"age":"40","sex":"F","smoke":"1","sbp":"180","chol":"8","risk":"low"};
+var scoreJS = rewire("../score10yrRisk");
+var score10yrRisk = scoreJS.__get__("score10yrRisk");
+var output = 0;
 
-var output = {"CHDRisk": NaN, "NonCHDRisk": NaN, "TotalRisk": NaN}
+test('Did it run', () => {
+  expect(score10yrRisk(inputs)).toEqual(expect.anything());
+});
+
 test('runs model', () => {
+
   expect(score10yrRisk(inputs)).toEqual(output);
 });
