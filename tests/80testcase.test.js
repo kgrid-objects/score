9821b6c5792b023d4e5088af3249e9b02c1f441f
Jack Allan <jackall@umich.edu>
2019-04-02 15:12:05 -0400

    hi

--- tests/80testcase.test.js
+++ tests/80testcase.test.js
@@ -8,20 +8,38 @@ const csvTestInputFilePath='./tests/input.csv'
 //const jsonArray=await csv().fromFile(csvTestInputFilePath);
 
 
-test('KO scores 80 test patients', () => {
-    
-        // expect(jsonArray).toEqual("");
-//        csv().fromFile(csvTestInputFilePath).then((jsonObj)=>{
-//            expect(jsonObj).toEqual("");
-//            /** 
-//             * [
-//             * 	{a:"1", b:"2", c:"3"},
-//             * 	{a:"4", b:"5". c:"6"}
-//             * ]
-//             */ 
-//        })
-    
-    return csv().fromFile(csvTestInputFilePath).then((jsonObj)=>{
-        expect(jsonObj).toBe("");
-    });
+test('KO scores 80 test patients', async () => {
+
+             //expect.assertions(1);
+             const jsonArray= await csv().fromFile(csvTestInputFilePath);
+             expect(jsonArray).toBe([]);
 });
+
+
+// this is how the csv file prints
+
+``` 
+Object {
+    +     "CHDRisk": "0.00595473",
+    +     "age": "65",
+    +     "chol": "4",
+    +     "nonCHDRisk": "0.008040825",
+    +     "risk": "low",
+    +     "sbp": "120",
+    +     "sex": "F",
+    +     "smoke": "N",
+    +     "totalRisk": "0.013995555",
+    +   },
+    +   Object {
+    +     "CHDRisk": "0.04489347",
+    +     "age": "65",
+    +     "chol": "8",
+    +     "nonCHDRisk": "0.032208835",
+    +     "risk": "low",
+    +     "sbp": "180",
+    +     "sex": "F",
+    +     "smoke": "N",
+    +     "totalRisk": "0.077102305",
+    +   },
+```
+
